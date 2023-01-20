f10037394e6ab51c301f2630610fbc6f084f0b10
jeevancandorcode <jeevan@candorcode.com>
2023-01-20 18:53:47 +0530

    .first commit

---
+++ build/precache-manifest.374865c5e155d67d2bb7f5706444a182.js
@@ -0,0 +1,462 @@
+self.__precacheManifest = [
+  {
+    "revision": "605d83d3f3f1b994bc24",
+    "url": "/static/css/main.70e401a0.chunk.css"
+  },
+  {
+    "revision": "605d83d3f3f1b994bc24",
+    "url": "/static/js/main.1347bf0d.chunk.js"
+  },
+  {
+    "revision": "b90f54f8119fe40f54b6",
+    "url": "/static/js/runtime~main.e20da15f.js"
+  },
+  {
+    "revision": "e869f110c0975895f4a3",
+    "url": "/static/js/2.df439cd7.chunk.js"
+  },
+  {
+    "revision": "843aee950870bf049a19",
+    "url": "/static/js/3.908fe8eb.chunk.js"
+  },
+  {
+    "revision": "93a024bcfad8690b761da76e5234aae9",
+    "url": "/static/media/productlistcart.93a024bc.svg"
+  },
+  {
+    "revision": "045e8bb6e7f54f1b65742225825a4710",
+    "url": "/static/media/berrybalm.045e8bb6.svg"
+  },
+  {
+    "revision": "6ba3111694b43203335b9e006bf660b9",
+    "url": "/static/media/bflymobile.6ba31116.svg"
+  },
+  {
+    "revision": "1993059415b73726bac15ebdf3105630",
+    "url": "/static/media/editicon.19930594.svg"
+  },
+  {
+    "revision": "308a7410ffee7820c2507a2ec5c40ec0",
+    "url": "/static/media/blankimage.308a7410.svg"
+  },
+  {
+    "revision": "fe5de29279d66851e75e72084839bd5a",
+    "url": "/static/media/elipse.fe5de292.svg"
+  },
+  {
+    "revision": "5fc659601b8a904d8938582349609bac",
+    "url": "/static/media/ellipse.5fc65960.svg"
+  },
+  {
+    "revision": "49d512cbb316bd660d8852fd1ea86400",
+    "url": "/static/media/userratingimage.49d512cb.svg"
+  },
+  {
+    "revision": "b564f6c54ea2e58ad95351986808efda",
+    "url": "/static/media/leaf1.b564f6c5.svg"
+  },
+  {
+    "revision": "cb5222d0e5e5161c9deae48308deb7c0",
+    "url": "/static/media/roundtext1.cb5222d0.svg"
+  },
+  {
+    "revision": "f9ff2930bbc5e29fc0301c82e1113bf9",
+    "url": "/static/media/bottleblack.f9ff2930.svg"
+  },
+  {
+    "revision": "fe245745ea719f8dbb6a4f55fde866f6",
+    "url": "/static/media/oatsimage.fe245745.svg"
+  },
+  {
+    "revision": "89456081a000c9a1c51dcfaaf5c8867e",
+    "url": "/static/media/blemishes.89456081.svg"
+  },
+  {
+    "revision": "6231e4144b20227f5aeaf1a3fb7f9611",
+    "url": "/static/media/unevenskin.6231e414.svg"
+  },
+  {
+    "revision": "0c6927a39cb4deba7471f53d038f6509",
+    "url": "/static/media/dryskin.0c6927a3.svg"
+  },
+  {
+    "revision": "8bf7d7f08173837de6749a403b250af1",
+    "url": "/static/media/frizzyhair.8bf7d7f0.svg"
+  },
+  {
+    "revision": "e7969a77d05edcac2ce70d93bebc6457",
+    "url": "/static/media/howstarted.e7969a77.svg"
+  },
+  {
+    "revision": "597d856ef676e12475733f28066432f3",
+    "url": "/static/media/plusellipse.597d856e.svg"
+  },
+  {
+    "revision": "402528b7b5ff480a0d8740fdfbcc9532",
+    "url": "/static/media/butterfly2.402528b7.svg"
+  },
+  {
+    "revision": "78357f11dd230a184f30dc863160540b",
+    "url": "/static/media/indiatoday.78357f11.svg"
+  },
+  {
+    "revision": "1243e80986d880d707aa5b31159adb62",
+    "url": "/static/media/popxo.1243e809.svg"
+  },
+  {
+    "revision": "e9dbf1f24fa82bd1f7c4fe437c9fbdde",
+    "url": "/static/media/bloghairoilpic.e9dbf1f2.svg"
+  },
+  {
+    "revision": "09245e2634cf9739af9b72b087b06609",
+    "url": "/static/media/myorderuncol.09245e26.svg"
+  },
+  {
+    "revision": "16422d0bdd747aa0567154d10a50caaf",
+    "url": "/static/media/ourstory.16422d0b.svg"
+  },
+  {
+    "revision": "903c03415dcd80b97199512dbd1d75f8",
+    "url": "/static/media/hairconditioner.903c0341.svg"
+  },
+  {
+    "revision": "3aef331e5e94199dc58963fa17808184",
+    "url": "/static/media/facecleaner.3aef331e.svg"
+  },
+  {
+    "revision": "23032ca344e74e0184a3035082ba0d5b",
+    "url": "/static/media/elipsminus.23032ca3.svg"
+  },
+  {
+    "revision": "cc762753a25b42f8f9e379a560db178f",
+    "url": "/static/media/myordertick.cc762753.svg"
+  },
+  {
+    "revision": "da2dd72bce0e5280428318888c22dfa6",
+    "url": "/static/media/myorderpic.da2dd72b.svg"
+  },
+  {
+    "revision": "7fbc4015fbfa0bed9504acca4f757450",
+    "url": "/static/media/logo.7fbc4015.svg"
+  },
+  {
+    "revision": "a66359ba7d8ea13ddb80871b51776b0b",
+    "url": "/static/media/search.a66359ba.svg"
+  },
+  {
+    "revision": "94a9f2fb45c8ee14e481b4a3baa107ae",
+    "url": "/static/media/cart.94a9f2fb.svg"
+  },
+  {
+    "revision": "6a34f49277b92024579ace41d3637661",
+    "url": "/static/media/profile.6a34f492.svg"
+  },
+  {
+    "revision": "ce2c200b19841bc0eeff28be58088867",
+    "url": "/static/media/closevector.ce2c200b.svg"
+  },
+  {
+    "revision": "e57f445fd802957d097e0fd82ae0b2c7",
+    "url": "/static/media/facebookicon.e57f445f.svg"
+  },
+  {
+    "revision": "cc55185bfb35ec2b4d229ec8f77ea3de",
+    "url": "/static/media/instaicon.cc55185b.svg"
+  },
+  {
+    "revision": "fa17a4eaa8327cf081d5ee04fefea949",
+    "url": "/static/media/twittericon.fa17a4ea.svg"
+  },
+  {
+    "revision": "a5ae4b2eb4bea1d21a5ea0788d70d1d0",
+    "url": "/static/media/whatsappicon.a5ae4b2e.svg"
+  },
+  {
+    "revision": "a34c543de38b64a37f9d30dfe6b32fba",
+    "url": "/static/media/butterfly.a34c543d.svg"
+  },
+  {
+    "revision": "39b977b5cee8af135ca829690322ac50",
+    "url": "/static/media/chatvector.39b977b5.svg"
+  },
+  {
+    "revision": "bb989c7db0de197ef1ec0c40a9cc78d4",
+    "url": "/static/media/fibrousoats.bb989c7d.svg"
+  },
+  {
+    "revision": "54b2241694a8fe0d453f4ff5fad224da",
+    "url": "/static/media/rosemary.54b22416.svg"
+  },
+  {
+    "revision": "33aecff74b1f2e59bf05be0bdb05df40",
+    "url": "/static/media/freshraspberry.33aecff7.svg"
+  },
+  {
+    "revision": "03fa06655555b8a104bcded952e3c820",
+    "url": "/static/media/vantablackhairoil.03fa0665.svg"
+  },
+  {
+    "revision": "d13103c1791247b0a87e9e2c33a323da",
+    "url": "/static/media/organic.d13103c1.svg"
+  },
+  {
+    "revision": "da3b6adf330c18ec5bb5d45e0eb04405",
+    "url": "/static/media/animals.da3b6adf.svg"
+  },
+  {
+    "revision": "1648312fee99aa9e69822835d2655c56",
+    "url": "/static/media/cruelyfree.1648312f.svg"
+  },
+  {
+    "revision": "df53bd2ee6ae594f1a348973e7230365",
+    "url": "/static/media/natural.df53bd2e.svg"
+  },
+  {
+    "revision": "96f371706bc67518271968ff7fcc380e",
+    "url": "/static/media/routine.96f37170.svg"
+  },
+  {
+    "revision": "b1c5b8dd710757d7faec95018a8ab660",
+    "url": "/static/media/routine_image.b1c5b8dd.png"
+  },
+  {
+    "revision": "1a165840aea5114ccbb0cb55f3fbe5c6",
+    "url": "/static/media/babysquare.1a165840.svg"
+  },
+  {
+    "revision": "ac6d115e3de0e8deaf29a686ef995240",
+    "url": "/static/media/bodysquare.ac6d115e.svg"
+  },
+  {
+    "revision": "98c7ff031bb292e22cb70fc440b4a907",
+    "url": "/static/media/facesquare.98c7ff03.svg"
+  },
+  {
+    "revision": "37bf2adfc402f6718567cbf95ab8cb52",
+    "url": "/static/media/hairsquare.37bf2adf.svg"
+  },
+  {
+    "revision": "9093d30fc0f1fc4d2197e634eed39a92",
+    "url": "/static/media/mensquare.9093d30f.svg"
+  },
+  {
+    "revision": "e24555f84a75235c124d00e096de3885",
+    "url": "/static/media/testimonialsback.e24555f8.svg"
+  },
+  {
+    "revision": "4b05ac398b52626fced85ee013401349",
+    "url": "/static/media/hairoil1.4b05ac39.svg"
+  },
+  {
+    "revision": "211e49bd45afa9a37952f432fe82f7e4",
+    "url": "/static/media/hairoil2.211e49bd.svg"
+  },
+  {
+    "revision": "a543a9662a62db66e1e77be53bed0233",
+    "url": "/static/media/hairoil3.a543a966.svg"
+  },
+  {
+    "revision": "81a556a792e0236ad8bf0a48e9cd7de6",
+    "url": "/static/media/homemobile.81a556a7.svg"
+  },
+  {
+    "revision": "e63539ed5b926279d7dd62d729543293",
+    "url": "/static/media/babymobileimage.e63539ed.svg"
+  },
+  {
+    "revision": "113222e7bd6860cb68f6f1d77a18b8ea",
+    "url": "/static/media/bodymobileimage.113222e7.svg"
+  },
+  {
+    "revision": "9cfa3bb9863f58a23cf69c2d66515e55",
+    "url": "/static/media/facemobileimage.9cfa3bb9.svg"
+  },
+  {
+    "revision": "0bf5c6d08c269c5d3a567d791903bdd4",
+    "url": "/static/media/hairmobileimage.0bf5c6d0.svg"
+  },
+  {
+    "revision": "bbb3d2b9a631c89e11f208f5a5b29b78",
+    "url": "/static/media/mensmobileimage.bbb3d2b9.svg"
+  },
+  {
+    "revision": "1d675c65c674f5c9adeda3713e2ec1fa",
+    "url": "/static/media/organicmobile.1d675c65.svg"
+  },
+  {
+    "revision": "0fb23b4859b30341babf28ef2c737c86",
+    "url": "/static/media/animalsmobile.0fb23b48.svg"
+  },
+  {
+    "revision": "aa0bfca1a9fb644f40a70d202cda3c35",
+    "url": "/static/media/naturalmobile.aa0bfca1.svg"
+  },
+  {
+    "revision": "823ed5bb94137fa0062dabcaf4ca20e6",
+    "url": "/static/media/crueltymobile.823ed5bb.svg"
+  },
+  {
+    "revision": "35e0455cffd9522fbc1ef14750c4bb8c",
+    "url": "/static/media/ourstoryleaf.35e0455c.svg"
+  },
+  {
+    "revision": "b1bcf6549b1906bf140b8ca2bad1ca73",
+    "url": "/static/media/rawbioimg1.b1bcf654.svg"
+  },
+  {
+    "revision": "e19c320831015a9327f055be5b3772b1",
+    "url": "/static/media/rawbioimg2.e19c3208.svg"
+  },
+  {
+    "revision": "09ba5e0e651a55b80e95a7679e2ea5ee",
+    "url": "/static/media/healnouri.09ba5e0e.svg"
+  },
+  {
+    "revision": "816d0329633799d742c47d634bd38d7d",
+    "url": "/static/media/formulated1.816d0329.svg"
+  },
+  {
+    "revision": "1d31f63f02bf325b42c7fed66850b07e",
+    "url": "/static/media/formulated2.1d31f63f.svg"
+  },
+  {
+    "revision": "5c5a6424020d8eddce929ef9df8f8ad1",
+    "url": "/static/media/formulattedbottle.5c5a6424.svg"
+  },
+  {
+    "revision": "ee45bf5e3d2f2288e326d9f96baead3c",
+    "url": "/static/media/uniquesoul.ee45bf5e.svg"
+  },
+  {
+    "revision": "427f8a5513b0ce29dbefc1a1febfc6b9",
+    "url": "/static/media/dryskincombo.427f8a55.svg"
+  },
+  {
+    "revision": "8447cc3f152ca6b4af97b396bb7b0e67",
+    "url": "/static/media/stone.8447cc3f.svg"
+  },
+  {
+    "revision": "d6db3200183e1817544a2069534539e2",
+    "url": "/static/media/slideimage1.d6db3200.svg"
+  },
+  {
+    "revision": "dcef7ed35e4734c6bb2f30aebe0c665f",
+    "url": "/static/media/slideimage2.dcef7ed3.svg"
+  },
+  {
+    "revision": "f8a75d90dd16195d75de3e73f9e360f1",
+    "url": "/static/media/slideimage3.f8a75d90.svg"
+  },
+  {
+    "revision": "1a2ef4031fad496a0c01ed708784e3f2",
+    "url": "/static/media/slideimage4.1a2ef403.svg"
+  },
+  {
+    "revision": "289e8154958be76a74521fba7f4ed574",
+    "url": "/static/media/texture.289e8154.svg"
+  },
+  {
+    "revision": "7d40e240800798b766142ceaa752008f",
+    "url": "/static/media/onyourskin.7d40e240.svg"
+  },
+  {
+    "revision": "c2dc8ab25c5b08575b6f5a3bce8f225d",
+    "url": "/static/media/backvector.c2dc8ab2.svg"
+  },
+  {
+    "revision": "d970b4a560993419864b17ff9b0d847c",
+    "url": "/static/media/forwardvector.d970b4a5.svg"
+  },
+  {
+    "revision": "5401890fd865ba580af872ea80821156",
+    "url": "/static/media/contactus.5401890f.svg"
+  },
+  {
+    "revision": "61743e7040570bd8b5cca205bdb4d5c5",
+    "url": "/static/media/blogpic.61743e70.svg"
+  },
+  {
+    "revision": "a31494c6079bc4c2bbc9eb1f7685265f",
+    "url": "/static/media/sidecartproduct.a31494c6.svg"
+  },
+  {
+    "revision": "cd07b9b1483326a5ee7e8ccaae16bcdb",
+    "url": "/static/media/close-x.cd07b9b1.svg"
+  },
+  {
+    "revision": "545ca315f10db94c4676737e85f16c82",
+    "url": "/static/media/facecleanser.545ca315.svg"
+  },
+  {
+    "revision": "b21b412966cfbb0c8724c91c59455022",
+    "url": "/static/media/receiptimage.b21b4129.svg"
+  },
+  {
+    "revision": "aed8888b2439a2c9baa848e91345cb90",
+    "url": "/static/media/orderconfirmedvector.aed8888b.svg"
+  },
+  {
+    "revision": "e1eb793b1337c5b2c0210961ffa9af75",
+    "url": "/static/media/myprofile.e1eb793b.svg"
+  },
+  {
+    "revision": "8344c19f828f0405ad1d3cfd0f480ebb",
+    "url": "/static/media/myorder.8344c19f.svg"
+  },
+  {
+    "revision": "ed59f72f790da73abef09153507cd15a",
+    "url": "/static/media/mywishlist1.ed59f72f.svg"
+  },
+  {
+    "revision": "0a5ed7393691d56d9afde18438449ecb",
+    "url": "/static/media/logout1.0a5ed739.svg"
+  },
+  {
+    "revision": "9025d6cd0faa94ad8919ebe9c6fa1238",
+    "url": "/static/media/myprofilevector.9025d6cd.svg"
+  },
+  {
+    "revision": "63bf8436a2c1174f13616b77637bb057",
+    "url": "/static/media/faqimage.63bf8436.svg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6c01fabbfc78702a8408b059ae3ad668",
+    "url": "/static/media/footerimage1.6c01fabb.svg"
+  },
+  {
+    "revision": "a75e02265a33c10741ab779d99e5ae0b",
+    "url": "/static/media/homeimage.a75e0226.svg"
+  },
+  {
+    "revision": "d51d92a68015013641f24cd92648a61b",
+    "url": "/static/media/favouritesicon.d51d92a6.svg"
+  },
+  {
+    "revision": "4c3e6ccbcf6ba529b0f7f104ff2cf02a",
+    "url": "/static/media/carticon.4c3e6ccb.svg"
+  },
+  {
+    "revision": "3be69f02bea98e95bc294fd2887573fe",
+    "url": "/static/media/inspirationbottles.3be69f02.svg"
+  },
+  {
+    "revision": "6cef657d631a1a8dd68c7f5376681806",
+    "url": "/static/media/inspirationscrub.6cef657d.svg"
+  },
+  {
+    "revision": "29b475a79ece90277b77dde76d101f4d",
+    "url": "/static/media/home1mobilebgt.29b475a7.svg"
+  },
+  {
+    "revision": "6ee676c916c393c5fdeef511fbb86bdf",
+    "url": "/static/media/routineimg.6ee676c9.svg"
+  },
+  {
+    "revision": "0546b6f0f563fa819cee2e46567d975e",
+    "url": "/index.html"
+  }
+];
